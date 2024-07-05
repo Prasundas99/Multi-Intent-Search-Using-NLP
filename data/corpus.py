448433f9f135b43159200f37933319512255016f
Prasundas99 <prasundas115@gmail.com>
2024-07-05 11:31:46 +0530

    init project

---
+++ data/corpus.py
@@ -0,0 +1,12 @@
+corpus = [
+    "Margherita Pizza", "Pepperoni Pizza", "Veggie Pizza",
+    "Classic Burger", "Cheese Burger", "Veggie Burger",
+    "California Roll", "Spicy Tuna Roll", "Avocado Roll",
+    "Spaghetti Bolognese", "Fettuccine Alfredo", "Penne Arrabbiata",
+    "Butter Chicken", "Paneer Tikka Masala", "Dal Makhani",
+    "Chicken Taco", "Beef Taco", "Veggie Taco",
+    "Caesar Salad", "Greek Salad", "Garden Salad",
+    "BBQ Chicken Wings", "BBQ Ribs", "Grilled Veggies",
+    "Classic Pancakes", "Blueberry Pancakes", "Chocolate Chip Pancakes",
+    "Strawberry Smoothie", "Mango Smoothie", "Green Smoothie"
+]
---
+++ data/searchData.py
@@ -0,0 +1,102 @@
+restraunts_data = [
+    {
+        "restaurant_id": 1,
+        "name": "Pizza Palace",
+        "menu": [
+            {"item_id": 101, "name": "Margherita Pizza", "price": 250},
+            {"item_id": 102, "name": "Pepperoni Pizza", "price": 300},
+            {"item_id": 103, "name": "Veggie Pizza", "price": 280}
+        ],
+        "distance_m": 500
+    },
+    {
+        "restaurant_id": 2,
+        "name": "Burger Bonanza",
+        "menu": [
+            {"item_id": 201, "name": "Classic Burger", "price": 150},
+            {"item_id": 202, "name": "Cheese Burger", "price": 180},
+            {"item_id": 203, "name": "Veggie Burger", "price": 160}
+        ],
+        "distance_m": 800
+    },
+    {
+        "restaurant_id": 3,
+        "name": "Sushi Central",
+        "menu": [
+            {"item_id": 301, "name": "California Roll", "price": 350},
+            {"item_id": 302, "name": "Spicy Tuna Roll", "price": 400},
+            {"item_id": 303, "name": "Avocado Roll", "price": 300}
+        ],
+        "distance_m": 1200
+    },
+    {
+        "restaurant_id": 4,
+        "name": "Pasta Point",
+        "menu": [
+            {"item_id": 401, "name": "Spaghetti Bolognese", "price": 320},
+            {"item_id": 402, "name": "Fettuccine Alfredo", "price": 350},
+            {"item_id": 403, "name": "Penne Arrabbiata", "price": 300}
+        ],
+        "distance_m": 200
+    },
+    {
+        "restaurant_id": 5,
+        "name": "Curry Corner",
+        "menu": [
+            {"item_id": 501, "name": "Butter Chicken", "price": 280},
+            {"item_id": 502, "name": "Paneer Tikka Masala", "price": 260},
+            {"item_id": 503, "name": "Dal Makhani", "price": 240}
+        ],
+        "distance_m": 600
+    },
+    {
+        "restaurant_id": 6,
+        "name": "Taco Town",
+        "menu": [
+            {"item_id": 601, "name": "Chicken Taco", "price": 120},
+            {"item_id": 602, "name": "Beef Taco", "price": 140},
+            {"item_id": 603, "name": "Veggie Taco", "price": 100}
+        ],
+        "distance_m": 900
+    },
+    {
+        "restaurant_id": 7,
+        "name": "Salad Stop",
+        "menu": [
+            {"item_id": 701, "name": "Caesar Salad", "price": 200},
+            {"item_id": 702, "name": "Greek Salad", "price": 180},
+            {"item_id": 703, "name": "Garden Salad", "price": 150}
+        ],
+        "distance_m": 300
+    },
+    {
+        "restaurant_id": 8,
+        "name": "BBQ Bliss",
+        "menu": [
+            {"item_id": 801, "name": "BBQ Chicken Wings", "price": 350},
+            {"item_id": 802, "name": "BBQ Ribs", "price": 500},
+            {"item_id": 803, "name": "Grilled Veggies", "price": 250}
+        ],
+        "distance_m": 1500
+    },
+    {
+        "restaurant_id": 9,
+        "name": "Pancake House",
+        "menu": [
+            {"item_id": 901, "name": "Classic Pancakes", "price": 150},
+            {"item_id": 902, "name": "Blueberry Pancakes", "price": 200},
+            {"item_id": 903, "name": "Chocolate Chip Pancakes", "price": 220}
+        ],
+        "distance_m": 700
+    },
+    {
+        "restaurant_id": 10,
+        "name": "Smoothie Shack",
+        "menu": [
+            {"item_id": 1001, "name": "Strawberry Smoothie", "price": 150},
+            {"item_id": 1002, "name": "Mango Smoothie", "price": 160},
+            {"item_id": 1003, "name": "Green Smoothie", "price": 180}
+        ],
+        "distance_m": 400
+    }
+]
